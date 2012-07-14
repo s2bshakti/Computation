cc9ca9824eaa3fa5ed9827f37fb136f675510572
Vinkesh Banka <vinkesh.banka@gmail.com>
2012-07-15 00:59:46 +0530

    Banka | added test case for substring problem as an example of simple test cases in c

--- sub_string.c
+++ sub_string.c
@@ -1,30 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-main()
-{
-	int a,len_sub,len_main;
-	char main[100];
-	char sub[100];
-	scanf("%s",main);
-	scanf("%s",sub);
-	len_sub=strlen(sub);
-	//len_main=strlen(main);
-	//printf("%d",len_main);
-	a=check_present(main,sub);	
-	if(a==len_sub)
-	{
-		printf("substring is present \n");
-	}
-	else
-	{
-		printf("substring is not present \n");
-	}
-}
-int check_present(char* main,char* sub)
-{
+#include<stdbool.h>
+
+int is_substring_present(char* main, char* sub){
 	int count=0;
 	char sub_sim='1',main_sim='2';
-	//printf("%c %c",sub_sim,main_sim);
+	int len_sub=strlen(sub);
 	while(*sub!='\0')
 	{
 		while(*main!='\0')
@@ -33,7 +15,6 @@ int check_present(char* main,char* sub)
 			{
 				main++;
 				count++;
-				printf("%d",count);
 				break;
 			}
 			else
@@ -41,8 +22,35 @@ int check_present(char* main,char* sub)
 		}
 		sub++;
 		if(*main!=*sub&&*sub!='\0')
-		return 0;	
+		return false;	
 	}	
-printf(" outside while %d",count);
-return count;
+	if(count == len_sub)
+		return true;
 }
+
+void assert_true(int value) {
+	if (value == false){
+		printf("Expected true, but was false");
+		exit(1);
+	}
+}
+
+void assert_false(int value) {
+	if (value == true){
+		printf("Expected false, but was true");
+		exit(1);
+	}
+}
+
+
+
+
+int main() {
+	int a,len_sub,len_main;
+	
+	assert_true(is_substring_present("abcdefg", "bcd"));
+	assert_true(is_substring_present("aabbccdd", "bc"));
+	assert_true(is_substring_present("aaabbbbccccdddd", "cd"));
+	assert_false(is_substring_present("aaabbbbccccdddd", "bcd"));
+}
+
