a61db73b701d5e416aaa601e716f04653e687cae
Shiv <shiv@ubuntu.ubuntu-domain>
2012-08-20 00:27:56 +0530

    Solution of NOCODING problem on codechef

---
+++ sol_nocoding.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+int no_operation(char*);
+main()
+{
+	int test,len_input,output[100],i,num;
+	char input[1000];
+	scanf("%d",&test);
+	for(i=0;i<test;i++)
+	{
+	scanf("%s",input);
+	len_input=strlen(input);
+	num=no_operation(input);
+	if(num>(len_input*11))
+		output[i]=0;
+	else
+		output[i]=1;
+	}
+	for(i=0;i<test;i++)
+	{
+		if(output[i]==0)
+		printf("NO\n");
+		else
+		printf("YES\n");	
+	}
+}
+int no_operation(char input[])
+{
+	int no_op=2,i=1,inc,temp0,temp1;
+	while(input[i]!='\0')
+	{
+		temp1=input[i];
+		temp0=input[i-1];
+		if(temp1 < temp0)
+		{
+			inc=(('z'-temp0)+(temp1-'a')+1);
+			no_op +=inc;
+			no_op++;
+		}		
+		else if(temp1>temp0)
+		{
+			inc=(temp1-temp0);
+			no_op+=inc;
+			no_op++;
+		}
+		else
+		{
+			no_op++;		
+		}
+		i++;	
+	}
+return no_op;
+}
+
